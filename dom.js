c5f8ff9d7117df758d2974c3328c4e76978cb2fb
Pavan-11 <Gpavankumar0005@gmail.com>
2022-12-28 12:30:39 +0530

    Querry-selector commit

--- dom.js
+++ dom.js
@@ -41,11 +41,42 @@
 
 //GETELEMENTBYTAGNAME
 
-var li = document.getElementsByTagName('li');
-li[2].style.backgroundColor = 'green';
-for(var i = 0;i <li.length;i++){
-    li[i].style.fontWeight = 'bold';
+// var li = document.getElementsByTagName('li');
+// li[2].style.backgroundColor = 'green';
+// for(var i = 0;i <li.length;i++){
+//     li[i].style.fontWeight = 'bold';
+
+// }
+
+
+//QuerrySELECTOR
+
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px grey'
+
+// var input = document.querySelector('input');
+// input.value = 'Hello World'
+
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value = 'SEND'
+
+// var item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
+
+
+// var SecondItem = document.querySelector('.list-group-item:nth-child(2)');
+// SecondItem.style.backgroundColor = 'green';
+
+
+// var ThirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// ThirdItem.style.display = 'None'
+
+
+var SecondItem = document.querySelectorAll('.list-group-item');
+SecondItem[1].style.color = 'green';
+
+var odd = document.querySelectorAll('.list-group-item:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor = 'green'
 
 }
-
-
