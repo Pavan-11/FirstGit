12a1010b50baab4036fbcc2390d5c11fc042d2ae
Pavan-11 <Gpavankumar0005@gmail.com>
2022-12-28 12:03:06 +0530

    second commit

--- dom.js
+++ dom.js
@@ -30,10 +30,22 @@
 //GETELEMENTBYCLASSNAME
 
 
-var items = document.getElementsByClassName('list-group-item');
+// var items = document.getElementsByClassName('list-group-item');
 
-items[2].style.backgroundColor = 'green';
-for(var i = 0;i< items.length;i++){
-    items[i].style.fontWeight = 'bold';
+// items[2].style.backgroundColor = 'green';
+// for(var i = 0;i< items.length;i++){
+//     items[i].style.fontWeight = 'bold';
+
+// }
+
+
+//GETELEMENTBYTAGNAME
+
+var li = document.getElementsByTagName('li');
+li[2].style.backgroundColor = 'green';
+for(var i = 0;i <li.length;i++){
+    li[i].style.fontWeight = 'bold';
 
 }
+
+
