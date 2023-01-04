cc7a624cea6b8efd9bc20c18f22766fc1d7167f8
Pavan-11 <Gpavankumar0005@gmail.com>
2023-01-04 16:43:08 +0530

    Storage-1

---
+++ userdetailstorage/StoreDetails.js
@@ -0,0 +1,35 @@
+
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit',onsubmit);
+
+function onsubmit(e){
+    e.preventDefault();
+    
+    const name = e.target.name.value
+    const email = e.target.email.value
+
+    localStorage.setItem('name',name)
+    localStorage.setItem('email',email)
+
+    
+    if(nameInput.value === '' || emailInput.value === ''){
+        msg.classList.add('error')
+        msg.innerHTML = 'please enter all fields';
+        setTimeout(() => msg.remove(),3000)
+    }else {
+        
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+        userList.appendChild(li);
+        
+        nameInput.value = '';
+        emailInput.value ='';
+
+    }
+}
